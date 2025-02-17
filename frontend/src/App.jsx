18d64eaa5b7891f36aafceda5e55d4ed4ac8c2f3
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-18 01:24:42 +0530

    setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 const App = () => {
-  return <div className="text-red-500">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
