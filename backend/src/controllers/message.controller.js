8d739cd3ed53ca65aef92d2af20b75aae0a3094d
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-18 00:24:15 +0530

    setup messageRoutes

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,5 @@
+export const getUsersForSidebar = async (req, res) => {};
+
+export const getMessages = async (req, res) => {};
+
+export const sendMessage = async (req, res) => {};
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 8001;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
