0c60587594f930f6bdc4bb7ef2637253fc993959
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-18 00:18:44 +0530

    implmented auth-checker api

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,6 +6,7 @@ import {
   login,
   logout,
   updateProfile,
+  checkAuth,
 } from "../controllers/auth.controller.js";
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
