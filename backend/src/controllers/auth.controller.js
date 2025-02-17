d28ba9ba54b2f213ef10b7f1139b2456147c9001
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-17 22:59:38 +0530

    setup authController template

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+  try {
+    return res.send("Hello");
+  } catch (error) {}
+};
+
+export const login = async (req, res) => {
+  try {
+    return res.send("Hello");
+  } catch (error) {}
+};
+
+export const logout = async (req, res) => {
+  try {
+    return res.send("Hello");
+  } catch (error) {}
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8001;
 
 app.use("/api/auth", authRoutes);
 
