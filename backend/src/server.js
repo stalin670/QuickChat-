0b840e685d83feb2c2c7f685da8d4e18719d0edb
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-19 03:08:33 +0530

    changes made : ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
@@ -9,6 +11,9 @@ import { connectDB } from "./lib/db.js";
 import { io, app, server } from "./lib/socket.js";
 
 dotenv.config();
+
+const PORT = process.env.PORT || 8001;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
@@ -20,10 +25,16 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 8001;
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,8 @@ import { axiosInstance } from "../lib/axiosInstance.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
