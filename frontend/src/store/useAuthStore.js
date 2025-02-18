f76f1561d392f06336f6e144c3cca59dad2c6cbe
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-18 17:39:30 +0530

    Build Login-UI-Page functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -44,4 +44,17 @@ export const useAuthStore = create((set) => ({
       toast.error(error.response.data.message);
     }
   },
+
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
 }));
