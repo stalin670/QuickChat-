9069d396487643ac04e1ab26656a0da92c87e68d
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-18 18:56:37 +0530

    build profilePage

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -57,4 +57,18 @@ export const useAuthStore = create((set) => ({
       set({ isLoggingIn: false });
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
