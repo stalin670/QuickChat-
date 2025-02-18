b62ca1a6b64685d7cb475eaa95d730be908c5e12
Amit Yadav <88823371+stalin670@users.noreply.github.com>
2025-02-18 17:33:26 +0530

    Build logout and Navbar UI

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -33,4 +33,15 @@ export const useAuthStore = create((set) => ({
       set({ isSigningUp: false });
     }
   },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+      get().disconnectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
 }));
